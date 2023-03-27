20bb4169b2a3c25c6b44e25ddb365121b29acc4e
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-03-27 13:52:43 +0500

    FOUNDATIONS: Components -> WIP

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-question',
   templateUrl: './question.component.html',
   styleUrls: ['./question.component.scss']
 })
-export class QuestionComponent {
+export class QuestionComponent implements OnInit {
 
+  public name: string = ""
+
+  ngOnInit(): void {
+    this.name = localStorage.getItem("name")!
+  }
 }
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.scss']
 })
-export class WelcomeComponent {
+export class WelcomeComponent implements OnInit {
 
+  @ViewChild('name') nameKey !: ElementRef
+
+  constructor(){}
+
+  ngOnInit(): void {
+
+  }
+
+  startGame(){
+    if(this.nameKey.nativeElement.value === ""){
+      alert("Please enter your name")
+    }
+    else{
+      localStorage.setItem("name", this.nameKey.nativeElement.value)
+    }
+
+  }
 }
