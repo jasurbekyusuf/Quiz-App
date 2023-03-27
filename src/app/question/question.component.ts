b31d671144f971c54947fb9de70adea109514c5a
Jasubek yusufov <jasurbekyusufov@gmail.com>
2023-03-27 18:21:43 +0500

    FOUNDATIONS: Question add RxJs

--- src/app/question/question.component.ts
+++ src/app/question/question.component.ts
@@ -1,5 +1,6 @@
 import { QuestionService } from './../service/question.service';
 import { Component, OnInit } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-question',
@@ -13,12 +14,17 @@ export class QuestionComponent implements OnInit {
   public currentQuestion: number =0
   public points: number = 0
   counter = 60
+  correctAnswer: number = 0
+  inCorrectAnswer: number = 0
+  interval$: any;
+  progress: string = "0"
 
   constructor(private questionService: QuestionService){}
 
   ngOnInit(): void {
     this.name = localStorage.getItem("name")!
     this.getAllQuestions()
+    this.startCounter()
   }
 
   getAllQuestions(){
@@ -34,5 +40,61 @@ export class QuestionComponent implements OnInit {
 
   prevQuestion() {
     this.currentQuestion--
+  }
+
+  answer(currentQno: number, option: any){
+    if(option.currect){
+      this.points+=10
+      this.correctAnswer++
+      this.currentQuestion++
+      this.resetCounter()
+      this.getProgressPercent()
+    }else{
+      this.currentQuestion++
+      this.inCorrectAnswer++
+      this.resetCounter()
+      this.getProgressPercent()
+      this.points -= 10
+    }
+  }
+
+  startCounter() {
+    this.interval$ = interval(1000)
+      .subscribe(val => {
+        this.counter--
+        if(this.counter === 0) {
+          this.currentQuestion++
+          this.counter = 60
+          this.points -=10
+        }
+      })
+      setTimeout(() =>{
+        this.interval$.unsubscribe()
+      },600000)
+  }
+
+  stopCounter(){
+    this.interval$.unsubscribe()
+    this.counter = 0
+  }
+
+  resetCounter(){
+    this.stopCounter()
+    this.counter = 60
+    this.startCounter()
+  }
+
+  resetGame(){
+    this.resetCounter()
+    this.getAllQuestions()
+    this.points = 0
+    this.counter = 60
+    this.currentQuestion = 0
+    this.progress = "0"
+  }
+
+  getProgressPercent() {
+    this.progress = ((this.currentQuestion/this.questionList.length)*100).toString()
+    return this.progress
   }
 }
